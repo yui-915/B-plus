0896e518b550354e67c3fc62071f13f0ce7c81f7
Yui <yui300127@gmail.com>
2025-05-27 14:20:21 +0300

    parse exit 0

--- bp/src/main.rs
+++ bp/src/main.rs
@@ -1,3 +1,149 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(unused, unused_variables, dead_code)]
+use {
+    anyhow::{Result, bail},
+    clap::Parser,
+    logos::{Logos, Span, SpannedIter},
+    std::{fs::read_to_string, iter::Peekable, ops::Range, path::PathBuf},
+};
+
+#[derive(Logos, Debug, PartialEq, Clone, Copy)]
+#[logos(skip r"[ \r\n\t]+")]
+enum Token {
+    #[token(",")]
+    Comma,
+    #[token(";")]
+    Semi,
+    #[token("(")]
+    LPar,
+    #[token(")")]
+    RPar,
+
+    #[regex(r"[_a-zA-Z][_a-zA-Z0-9]*")]
+    Ident,
+    #[regex(r"[0-9]+")]
+    Integer,
+}
+
+#[derive(Parser, Debug)]
+struct Cli {
+    file: PathBuf,
+}
+
+#[derive(Debug)]
+struct File {
+    funcs: Vec<Func>,
+}
+
+#[derive(Debug)]
+struct Func {
+    name: String,
+    body: Statement,
+}
+
+#[derive(Debug)]
+enum Statement {
+    FunCall(FunCall),
+}
+
+#[derive(Debug)]
+struct FunCall {
+    name: String,
+    args: Vec<RValue>,
+}
+
+#[derive(Debug)]
+enum RValue {
+    Constant(Constant),
+}
+
+#[derive(Debug)]
+enum Constant {
+    Integer(u64),
+}
+
+fn parse_file(c: &mut Compiler) -> Result<File> {
+    let mut file = File { funcs: vec![] };
+
+    while c.lexer.peek().is_some() {
+        file.funcs.push(parse_func(c)?);
+    }
+
+    Ok(file)
+}
+
+fn parse_func(c: &mut Compiler) -> Result<Func> {
+    let ident = c.expect(Token::Ident)?;
+    let name = c.source[ident.1].to_owned();
+    c.expect(Token::LPar)?;
+    c.expect(Token::RPar)?;
+    let body = parse_statement(c)?;
+    Ok(Func { name, body })
+}
+
+fn parse_statement(c: &mut Compiler) -> Result<Statement> {
+    let funcall = Statement::FunCall(parse_funcall(c)?);
+    c.expect(Token::Semi)?;
+    Ok(funcall)
+}
+
+fn parse_funcall(c: &mut Compiler) -> Result<FunCall> {
+    let ident = c.expect(Token::Ident)?;
+    let name = c.source[ident.1].to_owned();
+    let mut args = vec![];
+
+    c.expect(Token::LPar)?;
+    while let Ok(arg) = parse_rvalue(c) {
+        args.push(arg);
+    }
+    c.expect(Token::RPar)?;
+
+    Ok(FunCall { name, args })
+}
+
+fn parse_rvalue(c: &mut Compiler) -> Result<RValue> {
+    Ok(RValue::Constant(parse_constant(c)?))
+}
+
+fn parse_constant(c: &mut Compiler) -> Result<Constant> {
+    let (_, span) = c.expect(Token::Integer)?;
+    let int = c.source[span].parse()?;
+    Ok(Constant::Integer(int))
+}
+
+struct Compiler<'a> {
+    input_path: PathBuf,
+    source: String,
+    lexer: Peekable<SpannedIter<'a, Token>>,
+}
+
+impl<'a> Compiler<'a> {
+    #[must_use]
+    fn expect(&mut self, tok: Token) -> Result<(Token, Range<usize>)> {
+        let Some((_, span)) = self.lexer.peek() else {
+            bail!("expected `{:?}` but found `EOF`", tok);
+        };
+        let span = span.clone();
+        let ((Ok(token), _)) = self.lexer.peek().unwrap().clone() else {
+            bail!("expected `{:?}` but found `ERROR` as `{:?}`", tok, span);
+        };
+        if token != tok {
+            bail!("expected `{:?}` but found `{:?}` at {:?}", tok, token, span);
+        }
+        self.lexer.next();
+        Ok((token.clone(), span))
+    }
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+    let source = read_to_string(&cli.file)?;
+    let mut c = Compiler {
+        input_path: cli.file,
+        source: source.clone(),
+        lexer: Token::lexer(&source).spanned().peekable(),
+    };
+    let file = parse_file(&mut c)?;
+    println!("file: {file:#?}");
+
+    Ok(())
 }
